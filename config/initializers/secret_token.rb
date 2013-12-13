89219cf0a78569e5070a0e7672362cc9bdac9b35
Rodrigo Manhães <rmanhaes@gmail.com>
2013-12-13 18:43:28 -0200

    open house

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Academicrash::Application.config.secret_key_base = 'e432c2091ce8a3624b93ea983116a52f1c09ea1058b427554826e7ff90905c9ee05ee9d899b3ac23cbcbdd09d9ae4b30dfa0ba7620c6739824a4da02d1ef099b'
